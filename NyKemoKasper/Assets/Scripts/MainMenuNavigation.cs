5b38ccf10c827763ce57da3779a05c17f2e8e26a
Ludwik1526 <mmichal1526@gmail.com>
2020-03-23 19:57:13 +0100

    Navigation between menu

---
+++ NyKemoKasper/Assets/Scripts/MainMenuNavigation.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    public Button game1Button;
+    public Button game2Button;
+    public Button highscoresButton;
+    public Button comicsButton;
+    public Button eventsButton;
+    public Button profileButton;
+
+    public Button goBackButton;
+    public Button goToLoginMenuButton;
+
+    public GameObject mainCanvas;
+    public GameObject game1LaunchCanvas;
+    public GameObject game2LaunchCanvas;
+    public GameObject higscoresCanvas;
+    public GameObject comicsCanvas;
+    public GameObject eventsCanvas;
+    public GameObject profileCanvas;
+
+    void Start()
+    {
+        DisableAllCanvas();
+        mainCanvas.gameObject.SetActive(true);
+
+        goBackButton.onClick.AddListener(DisableAllCanvas);
+        game1Button.onClick.AddListener(OpenGame1LaunchCanvas);
+        game2Button.onClick.AddListener(OpenGame2LaunchCanvas);
+        highscoresButton.onClick.AddListener(OpenHigscoresCanvas);
+        comicsButton.onClick.AddListener(OpenComicsCanvas);
+        eventsButton.onClick.AddListener(OpenEventCanvas);
+        profileButton.onClick.AddListener(OpenProfileCanvas);
+        goToLoginMenuButton.onClick.AddListener(GoBackToLoginMenu);
+    }
+
+    public void DisableAllCanvas()
+    {
+        game1LaunchCanvas.gameObject.SetActive(false);
+        game2LaunchCanvas.gameObject.SetActive(false);
+        higscoresCanvas.gameObject.SetActive(false);
+        comicsCanvas.gameObject.SetActive(false);
+        eventsCanvas.gameObject.SetActive(false);
+        profileCanvas.gameObject.SetActive(false);
+        goBackButton.gameObject.SetActive(false);
+    }
+
+    public void OpenGame1LaunchCanvas()
+    {
+        game1LaunchCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenGame2LaunchCanvas()
+    {
+        game2LaunchCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenHigscoresCanvas()
+    {
+        higscoresCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenComicsCanvas()
+    {
+        comicsCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenEventCanvas()
+    {
+        eventsCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenProfileCanvas()
+    {
+        profileCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void GoBackToLoginMenu()
+    {
+        mainCanvas.gameObject.SetActive(false);
+        SceneManager.LoadScene("LoginMenu");
+    }
+}
---
+++ NyKemoKasper/Assets/Scripts/MainMenuNoLoginNavigation.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuNoLoginNavigation : MonoBehaviour
+{
+    public Button game1Button;
+    public Button game2Button;
+    public Button highscoresButton;
+    public Button comicsButton;
+    public Button eventsButton;
+
+    public Button goBackButton;
+    public Button goToLoginMenuButton;
+
+    public GameObject mainCanvas;
+    public GameObject game1LaunchCanvas;
+    public GameObject game2LaunchCanvas;
+    public GameObject higscoresCanvas;
+    public GameObject comicsCanvas;
+    public GameObject eventsCanvas;
+
+    void Start()
+    {
+        DisableAllCanvas();
+        mainCanvas.gameObject.SetActive(true);
+
+        goBackButton.onClick.AddListener(DisableAllCanvas);
+        game1Button.onClick.AddListener(OpenGame1LaunchCanvas);
+        game2Button.onClick.AddListener(OpenGame2LaunchCanvas);
+        highscoresButton.onClick.AddListener(OpenHigscoresCanvas);
+        comicsButton.onClick.AddListener(OpenComicsCanvas);
+        eventsButton.onClick.AddListener(OpenEventCanvas);
+        goToLoginMenuButton.onClick.AddListener(GoBackToLoginMenu);
+    }
+
+    public void DisableAllCanvas()
+    {
+        game1LaunchCanvas.gameObject.SetActive(false);
+        game2LaunchCanvas.gameObject.SetActive(false);
+        higscoresCanvas.gameObject.SetActive(false);
+        comicsCanvas.gameObject.SetActive(false);
+        eventsCanvas.gameObject.SetActive(false);
+        goBackButton.gameObject.SetActive(false);
+    }
+
+    public void OpenGame1LaunchCanvas()
+    {
+        game1LaunchCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenGame2LaunchCanvas()
+    {
+        game2LaunchCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenHigscoresCanvas()
+    {
+        higscoresCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenComicsCanvas()
+    {
+        comicsCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void OpenEventCanvas()
+    {
+        eventsCanvas.gameObject.SetActive(true);
+        goBackButton.gameObject.SetActive(true);
+    }
+
+    public void GoBackToLoginMenu()
+    {
+        mainCanvas.gameObject.SetActive(false);
+        SceneManager.LoadScene("LoginMenu");
+    }
+}

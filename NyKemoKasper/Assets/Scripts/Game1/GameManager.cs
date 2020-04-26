f1461b6a74066cfbede33a736335e923e7d56e61
Ludwik1526 <mmichal1526@gmail.com>
2020-04-27 00:29:10 +0200

    Game 1 start screens

--- NyKemoKasper/Assets/Scripts/Game1/GameManager.cs
+++ NyKemoKasper/Assets/Scripts/Game1/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,9 +12,22 @@ public class GameManager : MonoBehaviour
 
     private CellSpawner cellSpawner;
 
+    public GameObject startScreen, secondScreen;
+
+    public Button backButton, continueButton1, continueButton2;
+
     void Start()
     {
+        Time.timeScale = 0;
+
+        backButton.onClick.AddListener(GoBackToMenu);
+        continueButton1.onClick.AddListener(CloseFirstScreen);
+        continueButton2.onClick.AddListener(CloseSecondScreen);
+
         cellSpawner = FindObjectOfType<CellSpawner>();
+
+        startScreen.SetActive(true);
+        secondScreen.SetActive(false);
     }
     
     void Update()
@@ -24,5 +38,22 @@ public class GameManager : MonoBehaviour
     public void ReplicateCell(Transform origin)
     {
         cellSpawner.ReplicateGreenCell(origin);
+    }
+
+    void CloseFirstScreen()
+    {
+        startScreen.SetActive(false);
+        secondScreen.SetActive(true);
+    }
+
+    void CloseSecondScreen()
+    {
+        secondScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void GoBackToMenu()
+    {
+
     }
 }
